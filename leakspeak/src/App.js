24aa7bacad3fa310f3e349671acc58c0bd5881d6
EricBot89 <eloucks@gmail.com>
2019-12-07 18:15:33 -0500

    org view still needs styling

--- leakspeak/src/App.js
+++ leakspeak/src/App.js
@@ -4,29 +4,13 @@ import './App.css';
 //import Navbar and routes
 import Navbar from './components/Navbar'
 import Routes from './routes'
+import Org from './Org'
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Test here</Button>
-      </header>
-=======
       <Navbar />
       <Routes />
->>>>>>> bd528c3518ca470dafd9cb23ede0b2cf41864b5d
     </div>
   );
 }
--- leakspeak/src/Mint.js
+++ leakspeak/src/Mint.js
@@ -1,23 +1,46 @@
 import React from "react";
-import { Button, Header, Modal } from "semantic-ui-react";
+import { Button, Modal, Input } from "semantic-ui-react";
 
 class Mint extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isOpen: false,
       tokenQty: 0
     };
   }
 
+  handleChange = e => {
+    const { value } = e.target;
+    this.setState({ tokenQty: value });
+  };
+
   render() {
-    const {minter} = this.props
-    const {tokenQty} = this.state
+    const { minter } = this.props;
+    const { tokenQty } = this.state;
     return (
-      <Modal trigger={<Button>Mint Tokens</Button>}>
+      <Modal
+        trigger={
+          <Button onClick={() => this.setState({ isOpen: true })}>
+            Mint Tokens
+          </Button>
+        }
+        open={this.state.isOpen}
+      >
         <Modal.Header>How Many Tokens?</Modal.Header>
         <Modal.Content>
-          <input />
-          <Button onClick={(tokenQty) => minter(tokenQty)}>Confirm</Button>
+          <Input onChange={this.handleChange} />
+          <Modal.Actions>
+            <Button
+              onClick={() => {
+                minter(tokenQty);
+                this.setState({ isOpen: false });
+              }}
+            >
+              Confirm
+            </Button>
+            <Button onClick={() => this.setState({ isOpen: false })}>Cancel</Button>
+          </Modal.Actions>
         </Modal.Content>
       </Modal>
     );
