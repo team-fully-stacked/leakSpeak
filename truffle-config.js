791d1b9e1c332eba4718f9af78f05b2623dfc08c
EricBot89 <eloucks@gmail.com>
2019-12-07 21:31:16 -0500

    add a . to truffle build path

--- truffle-config.js
+++ truffle-config.js
@@ -25,7 +25,7 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  contracts_build_directory: '/leakspeak/src/contracts',
+  contracts_build_directory: './leakspeak/src/contracts',
   networks: {
     development: {
       host: '127.0.0.1', // Localhost (default: none)
